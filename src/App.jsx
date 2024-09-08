cd4c3f70830ffa9f233cdca07a665da1733933d2
Vishal Sharma <vsvishalsharma11@gmail.com>
2024-09-08 11:28:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header/Header'
+import './App.css'
+import { RouterProvider } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import About from "./components/About/About.jsx";
+import Home from "./components/Home/Home.jsx";
+import Contact from "./components/Contact.jsx";
+import Github, { githubInfoLoader } from "./components/Github.jsx";
+import User from "./components/User.jsx";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route loader={githubInfoLoader} path="/Github" element={<Github />} />
+      <Route path="user/:userid" element={<User />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
